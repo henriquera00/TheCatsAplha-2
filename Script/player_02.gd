85820aa59b1abffdf2d8d7cd76a2c66b94e839df
Henrique Andrade <chenriqueac00@gmail.com>
2025-05-22 21:25:27 -0300

    Atualização Animation Tree

--- Script/player_02.gd
+++ Script/player_02.gd
@@ -4,6 +4,7 @@ var state_machine
 var in_attack: bool = false
 var is_damage:bool = false
 var is_dead: bool = false
+
 @export_category("Variables")
 @export var speed: float = 100.0
 @export var accel: float = 0.8
@@ -20,63 +21,59 @@ var is_dead: bool = false
 func _ready() -> void:
 	animationtree.active = true
 	state_machine = animationtree["parameters/playback"]
-	
-	
+
 func _physics_process(_delta: float) -> void:
 	mover()
 	animated()
 	attack()
 	move_and_slide()
-	
+
 func mover() -> void:
 	var direction: Vector2 = Vector2(
 		Input.get_axis("ui_left", "ui_right"),
 		Input.get_axis("ui_up", "ui_down")
 	)
-	
+
 	if direction != Vector2.ZERO:
 		animationtree["parameters/Attack/blend_position"] = direction
 		animationtree["parameters/Idle/blend_position"] = direction
 		animationtree["parameters/Run/blend_position"] = direction
-		
+
 		velocity.x = lerp(velocity.x, direction.normalized().x * speed, accel)
 		velocity.y = lerp(velocity.y, direction.normalized().y * speed, accel)
 		return
-	
-		
+
 	velocity.x = lerp(velocity.x, 0.0, brake)
 	velocity.y = lerp(velocity.y, 0.0, brake)
-		
+
 	#velocity = direction.normalized() * speed
-	
+
 func attack() -> void:
 	if Input.is_action_just_pressed("attack") and not in_attack:
 		attack_timer.start()
 		in_attack = true
 
 func animated() -> void:
-	
+
 	if is_damage:
 		state_machine.travel("Dead")
-		
+
 	if in_attack:
 		state_machine.travel("Attack")
-		set_physics_process(false)		
+		set_physics_process(false)
 		return
 
 	if velocity.length() > 5:
 		state_machine.travel("Run")
 		return
-		
+
 	state_machine.travel("Idle")
-	
 	pass
 
 func _on_attack_timer_timeout() -> void:
-	set_physics_process(true)	
-	in_attack = false	
+	set_physics_process(true)
+	in_attack = false
 	pass # Replace with function body.
-
 
 #Função para dar Dano ao inimigo, ao entrar na área de contato
 func _on_attack_area_body_entered(body) -> void:
@@ -88,7 +85,7 @@ func update_health(damage) -> void:
 	life.size.x -= damage
 	print("DANO")
 	pass
-	
+
 func recovery():
 	if life.size.x <= 52:
 		regen_timer.start()
@@ -96,6 +93,7 @@ func recovery():
 		life.size.x = 52
 		print("Regeneração completa")
 	pass
+
 func _on_spawn_timer_timeout():
 	pass # Replace with function body.
 
@@ -112,7 +110,6 @@ func _on_damage_area_body_entered(body) -> void:
 			get_tree().reload_current_scene()
 			print("VOCÊ MORREU PATRÃO")
 	pass # Replace with function body.
-
 
 func _on_regen_delay_timeout():
 	life.size.x += regen
