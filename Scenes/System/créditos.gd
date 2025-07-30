9387ad0ca7b55ffd51c6d32ed3fd927fbd7f6460
Henrique Andrade <chenriqueac00@gmail.com>
2025-07-30 11:21:13 -0300

    Tela de Creditos funcional

---
+++ Scenes/System/créditos.gd
@@ -0,0 +1,8 @@
+extends Control
+
+
+
+
+func _on_voltar_pressed():
+	get_tree().change_scene_to_file("res://Scenes/System/menu.tscn")
+	pass # Replace with function body.
--- Scenes/System/menu.gd
+++ Scenes/System/menu.gd
@@ -2,5 +2,10 @@ extends Control
 
 
 func _on_jogar_pressed():
-	
+	get_tree().change_scene_to_file("res://Scenes/world_01.tscn")
+	pass # Replace with function body.
+
+
+func _on_créditos_pressed():
+	get_tree().change_scene_to_file("res://Scenes/System/créditos.tscn")
 	pass # Replace with function body.
